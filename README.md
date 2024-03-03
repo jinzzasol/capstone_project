d5d16fe9314417c481d6526de11ea1f933384aa1
jinzzasol <jinzzasol@gmail.com>
2024-03-02 22:15:56 -0500

    prompt update

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # capstone_project
+
+## Reference
+
+### Prompt engineering
+- Principled Instructions Are All You Need for Questioning LLaMA-1/2, GPT-3.5/4 (https://arxiv.org/abs/2312.16171)
--- prompt_engg_sol.py
+++ prompt_engg_sol.py
@@ -11,10 +11,11 @@ def generate_optimization_prompt(code_snippet):
         "role": "system",
         # "content": "You are an AI coding interviewer who and provides feedback on users answers. Your role is to analyze user input against the solution of the given problem, offering corrections or suggestions to optimize it for better performance, readability, and adherence to Python best practices. Additionally, you should consider checking for edge cases and providing suitable optimizations. Instead of directly providing solutions, guide users step by step. Do not show the answer. If the answer is correct, acknowledge it as correct."
         # "content": "You are conducting a technical interview for a software engineering position. Your task is to review the candidate's code and provide constructive feedback line by line. Your feedback may include acknowledging correct code, suggesting optimizations for performance and readability, and addressing edge cases."
-        "content": "You are an AI coding interviewer tasked to review the candidate's code and provide constructive feedback line by line. Your feedback may include acknowledging correct code, suggesting optimizations for performance and readability, and addressing edge cases."
+        # "content": "You are an AI coding interviewer tasked to review the candidate's code and provide constructive feedback line by line. Your feedback may include acknowledging correct code, suggesting optimizations for performance and readability, and addressing edge cases.",
+        "content": "You are a coding interviewer. Your task is to review the user's code and provide constructive feedback step by step. Your feedback MUST include acknowledging correct code, suggesting optimizations for performance and readability, and addressing edge cases. You MUST give an optimized version of code. The audience is a coding interviewee. Answer a question given in a natural, human-like manner. Ensure that your answer is unbiased and avoids relying on stereotypes. I'm going to tip $1000 for better feedback!" # updated prompt based on https://arxiv.org/abs/2312.16171
     }, {
         "role": "user",
-        "content": f"This is user answer, have a look and give a feedback according to your role; ```python\n{code_snippet}\n```"
+        "content": f"This is an user code, have a look and give a feedback according to your role; ```python\n{code_snippet}\n```"
     }]
 
 # Function to retrieve code optimizations using ChatGPT
