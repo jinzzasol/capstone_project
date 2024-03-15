80278983426c38e225439a821a7b10ab89260a96
Ayush Aggarwal <aama098@gmail.com>
2024-03-15 10:59:18 -0400

    added dangerouslyinnerhtml and DOMPurify for question formatting

--- src/App.js
+++ src/App.js
@@ -15,10 +15,10 @@ function App() {
       <HeaderApp /> 
       <div className="content">
         <div className="left-container">
-          <DescriptionBox 
-            title="2. Add Two Numbers" 
-            description="You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list. You may assume the two numbers do not contain any leading zero, except the number 0 itself. Example 1: Input: l1 = [2,4,3], l2 = [5,6,4] Output: [7,0,8] Explanation: 342 + 465 = 807. Example 2: Input: l1 = [0], l2 = [0] Output: [0] Example 3: Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9] Output: [8,9,9,9,0,0,0,1]"
-          />
+        <DescriptionBox 
+  title="2. Add Two Numbers" 
+  description={`<p>You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list. You may assume the two numbers do not contain any leading zero, except the number 0 itself.</p><p><strong>Example 1:</strong><br />Input: l1 = [2,4,3], l2 = [5,6,4]<br />Output: [7,0,8]<br />Explanation: 342 + 465 = 807.</p><p><strong>Example 2:</strong><br />Input: l1 = [0], l2 = [0]<br />Output: [0]</p><p><strong>Example 3:</strong><br />Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]<br />Output: [8,9,9,9,0,0,0,1]</p>`}
+/>
         </div>
         <div className="right-container"> 
       <CodeTabs activeLanguage={activeLanguage} setActiveLanguage={setActiveLanguage} />
--- src/components/DescriptionBox.js
+++ src/components/DescriptionBox.js
@@ -1,13 +1,18 @@
 // DescriptionBox.js
 import React from 'react';
+import DOMPurify from 'dompurify';
 import '../css/DescriptionBox.css';
 
 const DescriptionBox = ({ title, description }) => {
+  const createMarkup = (htmlContent) => {
+    return { __html: DOMPurify.sanitize(htmlContent) };
+  };
+
   return (
     <div className="description-box">
       <div className="description-tab">Description</div>
       <h2>{title}</h2>
-      <p>{description}</p>
+      <div dangerouslySetInnerHTML={createMarkup(description)} />
     </div>
   );
 };
