6cbc41cac9d9127d5da1d3cb0a010299228a2d2a
Ayush Aggarwal <aama098@gmail.com>
2024-03-16 17:52:40 -0400

    added questions with html tags, added navigation for each question

--- src/components/DescriptionBox.js
+++ src/components/DescriptionBox.js
@@ -3,7 +3,7 @@ import React from 'react';
 import DOMPurify from 'dompurify';
 import '../css/DescriptionBox.css';
 
-const DescriptionBox = ({ title, description }) => {
+const DescriptionBox = ({ title, description, onPreviousClick, onNextClick }) => {
   const createMarkup = (htmlContent) => {
     return { __html: DOMPurify.sanitize(htmlContent) };
   };
@@ -13,6 +13,10 @@ const DescriptionBox = ({ title, description }) => {
       <div className="description-tab">Description</div>
       <h2>{title}</h2>
       <div dangerouslySetInnerHTML={createMarkup(description)} />
+      <div className="description-navigation">
+        <button onClick={onPreviousClick}>Previous</button>
+        <button onClick={onNextClick}>Next</button>
+      </div>
     </div>
   );
 };
