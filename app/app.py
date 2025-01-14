585cd62f960d14196f029072acfe9230c07d20cf
parikshitpawde <parikshitpawde@vt.edu>
2025-01-14 21:03:41 +0530

    changes for np,m

--- app/app.py
+++ app/app.py
@@ -53,7 +53,13 @@ app.config['SESSION_COOKIE_SAMESITE'] = 'None'
 app.config['SESSION_COOKIE_SECURE'] = True
 
 app.config['SESSION_CACHELIB']=FileSystemCache(threshold=1000, cache_dir="/sessions")
-CORS(app, resources={r"/api/*": {"origins": "http://localhost:3000"}}, supports_credentials=True)
+CORS(app, resources={
+    r"/api/*": {
+        "origins": ["http://52.91.5.78:3000", "http://localhost:3000"],
+        "methods": ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+        "allow_headers": ["Content-Type", "Authorization"]
+    }
+})
 Session(app)
 
 
